c8e2ff3eff7508bac8c4e921b6994beb9f4ea93b
crisperi <phoenixedian@gmail.com>
2024-03-15 00:00:02 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution in this file!
+// Write your solution in this file!
+const employee = {
+    name : "Johny",
+    streetAddress: 'Juja'
+};
+
+
+function updateEmployeeWithKeyAndValue(employee, key, value ){
+let newEmployee = {...employee};
+
+newEmployee[key]= value;
+
+return newEmployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+  employee[key]= value ;
+
+}
+
+function deleteFromEmployeeByKey(employee,key){
+    const newEmployee = {...employee};
+
+    if (key in newEmployee){
+        delete newEmployee[key];
+    }
+
+    return newEmployee;
+
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key ){
+
+    if (key in employee){
+        delete employee[key];
+
+    }
+
+}
